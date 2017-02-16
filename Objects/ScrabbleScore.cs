b57a13653c2d8b9a79bcbd301e6731a0758bb98f
Alexandra Holcombe and Jiwon Kang <alexandra+jiwon@>
2017-02-16 09:38:46 -0800

    Add test for first spec, create ScrabbleScoreGenerator method

--- Objects/ScrabbleScore.cs
+++ Objects/ScrabbleScore.cs
@@ -5,6 +5,23 @@ namespace ScrabbleScoreApp
 {
   public class ScrabbleScore
   {
-    
+    private string _userInput;
+
+    public ScrabbleScore(string userInput)
+    {
+      _userInput = userInput;
+    }
+
+    public string GetUserInput()
+    {
+      return _userInput;
+    }
+
+    public int ScrabbleScoreGenerator()
+    {
+      string userInput = GetUserInput();
+
+      return 0;
+    }
   }
 }
--- Tests/ScrabbleScoreTest.cs
+++ Tests/ScrabbleScoreTest.cs
@@ -6,6 +6,14 @@ namespace ScrabbleScoreApp
 {
   public class ScrabbleScoreTest
   {
-    
+    [Fact]
+    public void ScrabbleScoreGenerator_ForUserInputNumber_0()
+    {
+      string userInput = "42";
+      ScrabbleScore testScrabbleScore = new ScrabbleScore(userInput);
+      int result = testScrabbleScore.ScrabbleScoreGenerator();
+      int expectedResult = 0;
+      Assert.Equal(expectedResult, result);
+    }
   }
 }
